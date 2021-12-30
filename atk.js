773441056f91e0f0acd2bfb0da80a854ae236f93
dwiki nuridhuha <dwiki.nuridhuha@gmail.com>
2021-12-30 20:32:43 +0700

    fix

--- atk.js
+++ atk.js
@@ -1,5 +1,3 @@
-const atk = (createAtkName, damage, isCritical) => {
-    return `${createAtkName} death ${isCritical ? damage * 2 : damage} damage!`
-}
+const atk = (createAtkName, damage, isCritical) => `${createAtkName} death ${isCritical ? damage * 2 : damage} damage!`;
 
 module.exports = atk;
